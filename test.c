ce2f67ce90c6899844edb6c5cb4f4d7be62eb2fb
Duanhao Zheng <JackZheng@Duanhaos-Macintosh.local>
2017-03-20 08:53:46 -0400

    test

--- test.c
+++ test.c
@@ -10,18 +10,24 @@ int main(void) {
 	int result = 0;
 
 	MPI_Init(NULL, NULL);
-	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
-	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	comm = MPI_COMM_WORLD;
+	MPI_Comm_size(comm, &comm_sz);
+	MPI_Comm_rank(comm, &my_rank);
 
-	if (my_rank != 0) {
-		MPI_Reduce(num, &result, 6, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
-	}	
-	else {
-		MPI_Reduce(num, &result, 6, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+	// if (my_rank != 0) {
+	// 	MPI_Reduce(num, &result, 6, MPI_INT, MPI_MIN, 0, comm);
+	// }	
+	// else {
+	// 	MPI_Reduce(num, &result, 6, MPI_INT, MPI_MIN, 0, comm);
+	// 	printf("%d\n", result);
+	// }
+	
+
+	MPI_Allreduce(num, &result, 1, MPI_INT, MPI_MIN, comm);
+	if (my_rank == 0) {
 		printf("%d\n", result);
 	}
-	
-	// MPI_Reduce(num, &result, 6, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+	// MPI_Reduce(num, &result, 6, MPI_INT, MPI_MIN, 0, comm);
 	// printf("%d\n", result);
 	MPI_Finalize();
 	return 0;
