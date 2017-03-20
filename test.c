41209b01bae3b85bdbe5e6542ace0caa164c563d
Duanhao Zheng <JackZheng@Duanhaos-Macintosh.local>
2017-03-20 08:45:22 -0400

    test

--- test.c
+++ test.c
@@ -3,7 +3,7 @@
 #include <math.h>
 #include <mpi.h>
 
-int main(int argc, char *argv[]) {
+int main(void) {
 	int comm_sz;
 	int my_rank;
 	int num[6] = {1, 2, 3, 4, 2, 1};
@@ -13,8 +13,9 @@ int main(int argc, char *argv[]) {
 	MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-	if (my_rank!= 0)
+	if (my_rank != 0) {
 		MPI_Reduce(num, &result, 6, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+	}	
 	else {
 		MPI_Reduce(num, &result, 6, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
 		printf("%d\n", result);
